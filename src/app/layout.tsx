51aca171efbde55deb824e609ebb245538b8e14d
ayortiz-journal <ayortiz.journal@gmail.com>
2026-04-17 21:08:08 -0400

    Create RootLayout component in layout.tsxfeat: add Zod environment configuration and runtime validation

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import '../env';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
